ab84dcbcdbde4917c2ed143336cb00454cac5f30
Pedrobolfute <pepenike-101@hotmail.com>
2025-11-17 22:50:44 -0300

    bug fix

--- play/room_01/carregar_cenario_01.sh
+++ play/room_01/carregar_cenario_01.sh
@@ -1,2 +1,4 @@
-source $engine_src/1/load_room_01.sh
-source $engine_src/1/welcome_room_01.sh
+#!/bin/bash
+my_base_dir=$(find "$HOME" -type d -name "bash_hunter" -print -quit 2>/dev/null)
+source $my_base_dir/engine/src/1/load_room_01.sh
+source $my_base_dir/engine/src/1/welcome_room_01.sh
