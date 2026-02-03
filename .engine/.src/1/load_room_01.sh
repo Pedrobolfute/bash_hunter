352d7d8c4c0f90fc17805d91c4c4014c11bf10a7
Pedrobolfute <pepenike-101@hotmail.com>
2026-02-03 07:04:42 -0300

    bug fix2 it was other thing, my_base_dir nd not file choose_boat

--- .engine/.src/1/load_room_01.sh
+++ .engine/.src/1/load_room_01.sh
@@ -29,8 +29,8 @@ escolher() {
         return 1
     fi
 
-    local allowed_base="$base_dir/play/room_01/para_o_mar/senhor_do_bonfim/feira_de_santana/salvador/terminal_nautico_de_salvador/Barcos"
-    local output_file="$base_dir/.engine/.out/1/choosed_boat.txt"
+    local allowed_base="$my_base_dir/play/room_01/para_o_mar/senhor_do_bonfim/feira_de_santana/salvador/terminal_nautico_de_salvador/Barcos"
+    local output_file="$my_base_dir/.engine/.out/1/choosed_boat.txt"
     local current_dir
     current_dir=$(pwd)
 
@@ -54,7 +54,7 @@ meu_barco() {
         return 1
     fi
 
-    local output_file="$base_dir/.engine/.out/1/choosed_boat.txt"
+    local output_file="$my_base_dir/.engine/.out/1/choosed_boat.txt"
     if [[ -f "$output_file" ]]; then
       if [[ $(wc -c < "$output_file") -le 1 ]]; then
           echo "você ainda não escolheu um barco." 
@@ -77,7 +77,7 @@ zarpar() {
         return 1
     fi
 
-    local out_dir="$base_dir/.engine/.out/1"
+    local out_dir="$my_base_dir/.engine/.out/1"
     local choosed_boat_file="$out_dir/choosed_boat.txt"
     local room2_dir="$my_base_dir/play/room_02"
     local current_dir_name
@@ -124,8 +124,8 @@ zarpar() {
         echo "🧭 Navegando para ROOM_2..."
         sleep 1s
         cd "$room2_dir" || { echo "❌ Erro ao navegar!"; return 1; }
-        mv "$choosed_boat_file" "$base_dir/.engine/.out/2"
-        echo "içar_âncora" > "$base_dir/.engine/.out/1/key.txt"
+        mv "$choosed_boat_file" "$my_base_dir/.engine/.out/2"
+        echo "içar_âncora" > "$my_base_dir/.engine/.out/1/key.txt"
 
         echo "🌊 Você agora está Iniciando sua jornada, marujo! 🌊"
         echo "🌊 Essa é a BAIA DE TODOS OS SANTOS! Vá ao mar.   🌊"
