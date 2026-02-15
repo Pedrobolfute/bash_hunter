0c08dafabeb4622b15ab00bbbb126c3879a17fce
Pedrobolfute <pepenike-101@hotmail.com>
2026-02-15 17:37:11 -0300

    fix: game_way

--- play/room_01/carregar_cenario_01.sh
+++ play/room_01/carregar_cenario_01.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo -n "Carregando "
-find "$HOME" -type d -name "bash_hunter" -print -quit > result.txt 2>/dev/null & 
+find "$HOME" -type d -name "bash_hunter" -print -quit > .game_way.txt 2>/dev/null & 
 PID=$!
 while kill -0 $PID 2>/dev/null; do
     echo -n "."
     sleep 0.5
 done
 echo " Bem-vindo!"
-my_base_dir=$(cat resultado.txt)
+my_base_dir=$(cat .game_way.txt)
 
 
 source $my_base_dir/.engine/.src/1/load_room_01.sh
