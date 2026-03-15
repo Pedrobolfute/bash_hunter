4d34892d4228762fffe4de665823dea5a77486a2
Pedrobolfute <pepenike-101@hotmail.com>
2026-03-15 17:03:19 -0300

    feat: room04 load test

---
+++ .engine/.out/4/map.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cat << "EOF"
+
+.
+├── bin
+│   └── bau
+│       ├── ajuda.txt
+│       ├── chave.txt
+│       └── mapa.txt
+├── etc
+│   └── bau
+│       ├── ajuda.txt
+│       ├── chave.txt
+│       └── mapa.txt
+├── home
+│   └── bau
+│       ├── ajuda.txt
+│       ├── mapa.txt
+│       └── teste.txt
+├── raiz
+│   └── bau
+│       ├── ajuda.txt
+│       ├── chave.txt
+│       └── mapa.txt
+└── tmp
+    └── bau
+        ├── ajuda.txt
+        ├── mapa.txt
+        └── teste.txt
+
+EOF
