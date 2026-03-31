a1284820e30c0974c7e0e1d7cba41bf9a0a07305
Pedrobolfute <pepenike-101@hotmail.com>
2026-03-30 21:54:43 -0300

    fix: room_05_test_fixed

--- .engine/.src/5/load_room_05.sh
+++ .engine/.src/5/load_room_05.sh
@@ -7,18 +7,18 @@ if [[ -z "$1" ]]; then
 fi
 
 dec(){
-  local in="pmfivhehi"
+  local in="nskehsv"
   echo "$in" | tr 'e-za-de-za-d' 'a-za-z'
 }
 
 correct_key=$(dec)
 
-# decr(){
-#   local in="evmwi"
-#   echo "$in" | tr 'e-za-de-za-d' 'a-za-z'
-# }
+decr(){
+  local in="evmwi"
+  echo "$in" | tr 'e-za-de-za-d' 'a-za-z'
+}
 
-# sub=$(decr)
+sub=$(decr)
 
 if [[ "$1" != "$correct_key" ]]; then
     echo "❌ Senha errada. Você não pode carregar o cenário."
@@ -39,7 +39,8 @@ pwdd(){
     Sala (room_05) carregada.
 
     Bem vindo, Jogador Marujo!
-    Aqui você vai precisar usar bem os comandos grep, | e wc. 
+    Aqui você vai precisar usar bem os comandos grep, | e wc.
+
     Para conseguir avançar, decifre o código dos marinheiros
     e vá ao encontro da chave do próximo room.
 
@@ -54,27 +55,13 @@ pwdd(){
   fi
 }
 
-if [[ -d "$dirfrom" ]]; then
-  # echo "$sub" | sudo -S mv "$dirfrom/map" "/bin" 2>/dev/null
-  # echo "$sub" | sudo -S mv "$dirfrom/bin/bau" "/bin" 2>/dev/null
-  # echo "$sub" | sudo -S mv "$dirfrom/etc/bau" "/etc" 2>/dev/null
-  # echo "$sub" | sudo -S mv "$dirfrom/raiz/bau" "/" 2>/dev/null
-  # echo "$sub" | sudo -S mv "$dirfrom/tmp/bau" "/tmp" 2>/dev/null
-  # echo "$sub" | sudo -S mv "$dirfrom/home/bau" "/home" 2>/dev/null
-
-  mv "$dirfrom/chave.txt" $dirto 2>/dev/null
-  mv "$dirfrom/HISTORIA.txt" $dirto 2>/dev/null
-  mv "$dirfrom/instrução.txt" $dirto 2>/dev/null
-  # Lembrando de conferir eprmissão readonly no HISTORIA.txt
-
-  echo "true" > $engine_out/5/loaded.txt
-  clear 
-  pwdd
-else
+if [ -e "$engine_out/5/loaded.txt" ]; then
   clear
-  echo "Fase já foi iniciada. 
+  echo "
   
-O código é a soma do resultado dessas questões:
+  Fase já foi iniciada. 
+  
+    O código é a soma do resultado dessas questões:
     1° Quantas vezes a palavra "brasil" aparece
     no arquivo HISTÓRIA.txt?
 
@@ -92,5 +79,20 @@ O código é a soma do resultado dessas questões:
     no total?
 
     A soma de todos os 05 resultados é o código para o arquivo
-    "chave.txt". E ai, qual é o código?"
+    "chave.txt". E ai, qual é o código?
+    
+    "
+else
+  if [[ -d "$dirfrom" ]]; then
+    mv "$dirfrom/chave.txt" $dirto 2>/dev/null
+    mv "$dirfrom/HISTORIA.txt" $dirto 2>/dev/null
+    mv "$dirfrom/instrução.txt" $dirto 2>/dev/null
+    echo "$sub" | sudo -S chown root:root $dirto/HISTORIA.txt
+
+    echo "true" > $engine_out/5/loaded.txt
+    clear 
+    pwdd
+  else
+    clear
+  fi
 fi
