ffbddd568fe95a193d19828cf2989d87c116f1d2
Pedrobolfute <pepenike-101@hotmail.com>
2026-04-03 15:25:59 -0300

    fix: debugged tempestade

--- .engine/.src/6/load_room_06.sh
+++ .engine/.src/6/load_room_06.sh
@@ -78,10 +78,9 @@ Siga o arquivo de instruções.
 else
   if [[ -d "$dirfrom" ]]; then
     source "$dirfrom/atravessar"
-    echo "cabei execultar source"
     echo "$sub" | sudo -S mv "$dirfrom/find.txt" $dirto 2>/dev/null
     echo "$sub" | sudo -S mv "$dirfrom/instrução" $dirto 2>/dev/null
-    echo "$sub" | sudo -S mv "$dirfrom/tempestade/" 
+    echo "$sub" | sudo -S mv "$dirfrom/tempestade/" $dirto 2>/dev/null
 
     echo "$sub" | sudo -S echo "true" > $engine_out/6/loaded.txt
     # clear 
