5a2d1e2fc9b16d94d63e3ae4988434b1c90fb4bb
Pedrobolfute <pepenike-101@hotmail.com>
2026-04-04 09:27:03 -0300

    test: gateman test to free port

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect
 import subprocess
 import random
+import threading
+import time
 
 app = Flask(__name__)
 
@@ -15,6 +17,21 @@ def get_free_port():
             return port
     return None
 
+def monitor_container(container_name, port):
+    while True:
+        result = subprocess.run(
+            ["docker", "ps", "-q", "-f", f"name={container_name}"],
+            capture_output=True,
+            text=True
+        )
+
+        if not result.stdout.strip():
+            # container morreu
+            used_ports.discard(port)
+            print(f"[FREE] Porta {port} liberada")
+            break
+
+        time.sleep(2)
 
 @app.route("/")
 def index():
@@ -33,6 +50,12 @@ def index():
         "--rm",
         "bash_hunter_image"
     ])
+    
+    threading.Thread(
+      target=monitor_container,
+      args=(container_name, port),
+      daemon=True
+    ).start()
 
     # redireciona jogador
     return redirect(f"http://SEU_IP:{port}")
