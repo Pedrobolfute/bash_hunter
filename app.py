c24d7ab48350345945f9bb6e7c8ff89848805f4f
Pedrobolfute <pepenike-101@hotmail.com>
2026-04-04 14:44:59 -0300

    added middleware to see ip from client

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, redirect
+from werkzeug.middleware.proxy_fix import ProxyFix
 import subprocess
 import random
 import threading
 import time
 
 app = Flask(__name__)
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 PORT_RANGE = list(range(10000, 10101))
 used_ports = set()
