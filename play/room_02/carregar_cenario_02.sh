9181aca65d6c7ff5bb48cbff5b786205d4a17b4d
Pedrobolfute <pepenike-101@hotmail.com>
2026-04-05 11:46:27 -0300

    fix: bug not loaded room_02

--- play/room_02/carregar_cenario_02.sh
+++ play/room_02/carregar_cenario_02.sh
@@ -1,2 +1,6 @@
 echo "Iniciando..."
-source $engine_src/2/load_room_02.sh
+
+if ! source "$engine_src/2/load_room_02.sh" 2>/dev/null; then
+    echo "Erro ao carregar room_02."
+    echo "Por hora, recarregue o jogo apertando ctrl + f5"
+fi
--- play/room_02/minha_senha.sh
+++ play/room_02/minha_senha.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-source $engine_src/2/load_room_02_pw.sh
+echo "Iniciando..."
+
+if ! source $engine_src/2/load_room_02_pw.sh 2>/dev/null; then
+    echo "Erro ao carregar room_02."
+    echo "Por hora, recarregue o jogo apertando ctrl + f5"
+fi
