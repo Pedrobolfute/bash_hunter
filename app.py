81b2ea4c24aaa3e693bdecfc7c42879f30bb76c2
Pedrobolfute <pepenike-101@hotmail.com>
2026-04-11 19:12:57 -0300

    refact: I added certbot

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def index():
       ).start()
 
       if wait_for_port(port):
-        return redirect(f"http://bashhunter.com.br/play/{port}/")
+        return redirect(f"/play/{port}/")
       else:
         subprocess.run(["docker", "stop", container_name], capture_output=True)
         with ports_lock:
