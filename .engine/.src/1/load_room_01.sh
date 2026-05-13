e0dc86f94fe5d2d9351b384622cb767db0aab123
Pedrobolfute <pepenike-101@hotmail.com>
2026-05-13 06:53:05 -0300

    test 02

--- .engine/.src/1/load_room_01.sh
+++ .engine/.src/1/load_room_01.sh
@@ -148,8 +148,11 @@ decr(){
 }
 
 sub=$(decr)
+echo $sub
+
+echo ""$sub" | sudo -S mv "$engine_out/1/mapa" "/bin""
 echo "$sub" | sudo -S mv "$engine_out/1/mapa" "/bin"
-  
+
 # cd $play_dir/room_01/para_o_mar
 
 echo "true" > $my_base_dir/.engine/.out/1/loaded.txt
