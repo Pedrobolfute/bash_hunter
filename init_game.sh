590b4e33dddca3e04eded99f57195b0a766c88ef
Pedrobolfute <pepenike-101@hotmail.com>
2026-06-03 21:06:08 -0300

    change menssagem

--- init_game.sh
+++ init_game.sh
@@ -3,7 +3,7 @@
 my_base_dir=$(find "$HOME" -type d -name "bash_hunter" -print -quit 2>/dev/null) 
 
 source $my_base_dir/.engine/.src/1/load_room_01.sh
-# source $my_base_dir/.engine/.src/1/welcome_room_01.sh
+source $my_base_dir/.engine/.src/1/welcome_room_01.sh
 
 cd $my_base_dir/play/room_01
 exec bash
