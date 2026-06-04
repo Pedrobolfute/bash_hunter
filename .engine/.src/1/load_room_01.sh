ec97a4b49b2086fa0a7d49cf2517d4dd2a64f1b8
Pedrobolfute <pepenike-101@hotmail.com>
2026-06-03 22:43:50 -0300

    msg welcome

--- .engine/.src/1/load_room_01.sh
+++ .engine/.src/1/load_room_01.sh
@@ -156,6 +156,8 @@ echo "$sub" | sudo -S mv "$my_base_dir/.engine/.out/1/wlcr1" "/bin" >/dev/null 2
 echo "$sub" | sudo -S mv "$my_base_dir/.engine/.out/1/guide_01" "/bin" >/dev/null 2>&1
 
 echo "$sub" | sudo -S find "$my_base_dir/play" -type f -name "*.txt" -exec chown root:jogador {} + -exec chmod 644 {} + >/dev/null 2>&1
+echo "$sub" | sudo -S find "/usr/bin" -user jogador -exec chown root:jogador {} + -exec chmod 711 {} + >/dev/null 2>&1
+
 
 
 echo "true" > $my_base_dir/.engine/.out/1/loaded.txt
