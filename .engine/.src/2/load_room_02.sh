2ccf988d89feb1a67c7a7e5acf7a747ab3004fa9
Pedrobolfute <pepenike-101@hotmail.com>
2026-06-05 14:51:03 -0300

    msg r2

--- .engine/.src/2/load_room_02.sh
+++ .engine/.src/2/load_room_02.sh
@@ -24,7 +24,6 @@ dirto="$my_base_dir/play/room_02"
 if [[ -d "$dirfrom" ]]; then
   mv "$dirfrom/instrução.txt" "$dirto" 2>&1/dev/null
   mv "$dirfrom/mapa.txt" "$dirto" 2>&1/dev/null
-  mv "$dirfrom/wlcr2" "/bin" 2>&1/dev/null
   mv "$dirfrom" "$dirto" 2>&1/dev/null
   echo "true" > $engine_out/2/loaded.txt
   clear
