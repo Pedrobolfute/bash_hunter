32d40a2a37b49dd5da3b2aff52ad7aa40cbf5970
Pedrobolfute <pepenike-101@hotmail.com>
2026-06-05 07:45:04 -0300

    msg room2

--- .engine/.src/2/load_room_02.sh
+++ .engine/.src/2/load_room_02.sh
@@ -3,60 +3,23 @@
 finished=$( (tr -d '\r\n ' < "$engine_out/1/finished.txt") 2>/dev/null )
 
 if [[ "$finished" != "true" ]]; then
-  echo "
-    ⚠️ O room_01 precisa ser finalizado antes de prosseguir!
-  "
+  wlcr2 room_01_not_finished
   return 1
 fi
 
 if [[ -z "$1" ]]; then
-    echo "
-      ❌ Nenhuma senha informada. Informe a senha como no Exemplo abaixo:
-      "
-    echo "
-      source carregar_cenario_02.sh \"senha_aqui\"
-      "
+    wlcr2 pass_not_informed
     return 1
 fi
 
 correct_key=$(tr -d '\r\n ' < "$my_base_dir/.engine/.out/1/key.txt" 2>/dev/null)
 if [[ "$1" != "$correct_key" ]]; then
-    echo "
-      ❌ Senha errada. Você não pode carregar o cenário.
-      "
+    wlcr2 wrong_pass_informed
     return 1
 fi
 
 dirfrom="$my_base_dir/.engine/.out/2/event/baia_de_todos_os_santos"
 dirto="$my_base_dir/play/room_02"
-
-pwdd(){
-  local mensagem="
-    ▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
-    ▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
-    ▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
-
-    === SALA CARREGADA ===
-    Você entrou na fase 02 (room_02)!
-    Um novo caminho foi aberto...
-
-    === PRÓXIMO PASSO ===
-    Digite:
-    ls
-
-    Veja o que apareceu
-    e escolha para onde ir.
-
-    === MISSÃO ===
-    Explore o novo caminho!
-"
-
-  if command -v whiptail >/dev/null 2>&1; then
-    whiptail --title "🏴‍☠️ BASH HUNTER ⚓" --msgbox "$mensagem" 25 80
-  else
-    echo -e "\n$mensagem\n"
-  fi
-}
 
 if [[ -d "$dirfrom" ]]; then
   mv "$dirfrom/instrução.txt" "$dirto" 2>/dev/null
@@ -64,24 +27,8 @@ if [[ -d "$dirfrom" ]]; then
   mv "$dirfrom" "$dirto" 2>/dev/null
   echo "true" > $engine_out/2/loaded.txt
   clear
-  pwdd
+  wlcr2 default
 else
   clear
-  echo "
-    === FASE JÁ INICIADA ===
-    Você já está na fase 02 (room_02).
-
-
-    === PRÓXIMO PASSO ===
-    Digite:
-    ls
-
-    Veja os novos arquivos
-    e escolha o que fazer.
-
-
-    === DICA ===
-    Sempre use "ls"
-    para descobrir caminhos novos.
-    "
+  wlcr2 alread_started
 fi
--- .engine/.src/2/welcome_room_02.sh
+++ .engine/.src/2/welcome_room_02.sh
@@ -10,70 +10,7 @@ else
   sss=""
 fi
 
-welcome(){
-  local mensagem="
+wlcr2 wlc_room_02
 
-▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
-▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
-▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
-                       
-
-🪶 BEM-VINDO À FASE 02 (room_02)
-
-Marujo...
-
-Navegar parece fácil,
-mas você ainda não chegou ao mar aberto.
-
-Você está na Baía de Todos os Santos.
-
-Aqui é o lugar para treinar.
-
-Aprenda a navegar melhor
-e a usar os recursos do seu barco.
-
-"
-
-  if command -v whiptail >/dev/null 2>&1; then
-    whiptail --title "🏴‍☠️ BEM-VINDO AO BASH HUNTER ⚓" --msgbox "$mensagem$sss" 25 80
-  else
-    echo -e "\n$mensagem\n"
-  fi
-}
-
-clear
-welcome
-
-
-
-echo -e "
-▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
-▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
-▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
-
-=== GUIA RÁPIDO ===
-
-Use estes comandos para explorar o sistema:
-
-[1] ENTRAR EM UMA PASTA
-cd nome_da_pasta
-
-[2] VOLTAR UMA PASTA
-cd ..
-
-[3] VER ONDE VOCÊ ESTÁ
-pwd
-
-[4] VER ARQUIVOS E PASTAS
-ls
-
-[5] LER UM ARQUIVO
-cat arquivo.txt
-
-[6] ATIVAR UM SCRIPT
-source arquivo.sh
-
-▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
-▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
-▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
-                       "
+echo $sss
+wlcr2 wlc_room_02_fast_guide
