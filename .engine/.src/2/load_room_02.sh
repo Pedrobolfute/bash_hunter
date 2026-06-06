a549cbb07e1551e0ad2285806ab802426a901a20
Pedrobolfute <pepenike-101@hotmail.com>
2026-06-06 17:22:42 -0300

    r2-bugs fixe test

--- .engine/.src/2/load_room_02.sh
+++ .engine/.src/2/load_room_02.sh
@@ -26,14 +26,9 @@ sub=$(decr)
 
 dirfrom="$my_base_dir/.engine/.out/2/event/baia_de_todos_os_santos"
 dirto="$my_base_dir/play/room_02"
-echo "entres de entrar no main dir"
 if [[ -d "$dirfrom" ]]; then
-  echo "dentro main dir"
-  echo $(sub)
-  echo "antes mv bin"
   echo "$sub" | sudo -S mv -f "$dirfrom/mapa" "/bin" # 2>&1/dev/null
   echo "$sub" | sudo -S mv -f "$dirfrom/sos" "/bin" #2>&1/dev/null
-  echo "depois mv bin"
   mv "$dirfrom/instrução.txt" "$dirto" 2>&1/dev/null
   mv "$dirfrom/mapa.txt" "$dirto" 2>&1/dev/null
   mv "$dirfrom" "$dirto" 2>&1/dev/null
