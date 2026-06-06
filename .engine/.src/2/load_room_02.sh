cdbd63b53c31ab7aac9afec8a48cac788de13eaf
Pedrobolfute <pepenike-101@hotmail.com>
2026-06-06 03:04:25 -0300

    sos and mapa r2

--- .engine/.src/2/load_room_02.sh
+++ .engine/.src/2/load_room_02.sh
@@ -24,6 +24,8 @@ dirto="$my_base_dir/play/room_02"
 if [[ -d "$dirfrom" ]]; then
   mv "$dirfrom/instrução.txt" "$dirto" 2>&1/dev/null
   mv "$dirfrom/mapa.txt" "$dirto" 2>&1/dev/null
+  mv -f "$dirfrom/mapa" "/bin" 2>&1/dev/null
+  mv -f "$dirfrom/sos" "/bin" 2>&1/dev/null
   mv "$dirfrom" "$dirto" 2>&1/dev/null
   echo "true" > $engine_out/2/loaded.txt
   clear
