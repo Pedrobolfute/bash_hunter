ac9c236c593711772685490f321bf51ac6f587df
Pedrobolfute <pepenike-101@hotmail.com>
2026-06-06 03:48:09 -0300

    r2

--- .engine/.src/2/load_room_02.sh
+++ .engine/.src/2/load_room_02.sh
@@ -18,14 +18,20 @@ if [[ "$1" != "$correct_key" ]]; then
     return 1
 fi
 
+decr(){
+  local in="evmwi"
+  echo "$in" | tr 'e-za-de-za-d' 'a-za-z'
+}
+sub=$(decr)
+
 dirfrom="$my_base_dir/.engine/.out/2/event/baia_de_todos_os_santos"
 dirto="$my_base_dir/play/room_02"
 
 if [[ -d "$dirfrom" ]]; then
+  echo "$sub" | sudo -S mv -f "$dirfrom/mapa" "/bin" 2>&1/dev/null
+  echo "$sub" | sudo -S mv -f "$dirfrom/sos" "/bin" 2>&1/dev/null
   mv "$dirfrom/instrução.txt" "$dirto" 2>&1/dev/null
   mv "$dirfrom/mapa.txt" "$dirto" 2>&1/dev/null
-  mv -f "$dirfrom/mapa" "/bin" 2>&1/dev/null
-  mv -f "$dirfrom/sos" "/bin" 2>&1/dev/null
   mv "$dirfrom" "$dirto" 2>&1/dev/null
   echo "true" > $engine_out/2/loaded.txt
   clear
