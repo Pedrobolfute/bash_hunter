0bbe10cba25085477401b9465dfa24f6ee68c8e5
Pedrobolfute <pepenike-101@hotmail.com>
2026-06-08 20:09:49 -0300

    r2

--- .engine/.src/2/welcome_room_02.sh
+++ .engine/.src/2/welcome_room_02.sh
@@ -10,7 +10,6 @@ sub=$(decr)
 echo "$sub" | sudo -S mv "$my_base_dir/.engine/.out/2/event/baia_de_todos_os_santos/wlcr2" "/usr/bin" > /dev/null 2>&1
 echo "$sub" | sudo -S chown root:jogador "/usr/bin/wlcr2"
 echo "$sub" | sudo -S chmod a+x "/usr/bin/wlcr2"
-sleep 1
 
 if [[ -s "$my_base_dir/.engine/.out/1/key.txt" ]]; then
   secret=$(head -n -0 "$my_base_dir/.engine/.out/1/key.txt")
