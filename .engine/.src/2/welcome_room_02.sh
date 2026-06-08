747dad68526fe35002094f56345c6741a7ffac8b
Pedrobolfute <pepenike-101@hotmail.com>
2026-06-08 07:24:17 -0300

    bug fix r3

--- .engine/.src/2/welcome_room_02.sh
+++ .engine/.src/2/welcome_room_02.sh
@@ -7,8 +7,8 @@ decr(){
   echo "$in" | tr 'e-za-de-za-d' 'a-za-z'
 }
 sub=$(decr)
-echo "$sub" | sudo -S mv "$my_base_dir/.engine/.out/2/event/baia_de_todos_os_santos/wlcr2" "/bin" >/dev/null 2>&1
-sleep 0.5
+echo "$sub" | sudo -S mv "$my_base_dir/.engine/.out/2/event/baia_de_todos_os_santos/wlcr2" "/bin" #>/dev/null 2>&1
+sleep 1
 
 if [[ -s "$my_base_dir/.engine/.out/1/key.txt" ]]; then
   secret=$(head -n -0 "$my_base_dir/.engine/.out/1/key.txt")
