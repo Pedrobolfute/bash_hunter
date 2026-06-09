b5206e3f82d1d610be046c72dd40d4fdc12ae70f
Pedrobolfute <pepenike-101@hotmail.com>
2026-06-09 01:09:51 -0300

    r3-msg ok

--- .engine/.src/3/load_room_03.sh
+++ .engine/.src/3/load_room_03.sh
@@ -4,25 +4,52 @@ finished=$( (tr -d '\r\n ' < "$engine_out/2/finished.txt") 2>/dev/null )
 
 if [[ "$finished" != "true" ]]; then
   echo "
-    ⚠️ O room_02 precisa ser finalizado antes de prosseguir!
-  "
+▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
+▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
+▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
+
+⚠️ O room_02 precisa ser finalizado antes de prosseguir!
+
+▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
+▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
+▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
+
+"
   return 1
 fi
 
 if [[ -z "$1" ]]; then
     echo "
-      ❌ Nenhuma senha informada. Informe a senha como no Exemplo abaixo:"
-    echo "
-      source carregar_cenario_02.sh \"senha_aqui\"
-    "
+▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
+▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
+▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
+
+❌ Nenhuma senha informada. Informe a senha como no Exemplo abaixo:
+
+source carregar_cenario_03.sh \"senha_aqui\"
+
+▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
+▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
+▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
+
+"
     return 1
 fi
 
 correct_key=$(tr -d '\r\n ' < "$my_base_dir/.engine/.out/2/key.txt" 2>/dev/null)
 if [[ "$1" != "$correct_key" ]]; then
     echo "
-      ❌ Senha errada. Você não pode carregar o cenário.
-      "
+▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
+▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
+▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
+
+❌ Senha errada. Você não pode carregar o cenário.
+
+▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
+▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
+▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
+
+"
     return 1
 fi
 
@@ -31,24 +58,29 @@ dirto="$my_base_dir/play/room_03"
 
 pwdd(){
   local mensagem="
-    ▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
-    ▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
-    ▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
+▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
+▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
+▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
 
-    === SALA CARREGADA ===
-    Você entrou na fase 03 (room_03)!
+=== SALA CARREGADA ===
+Você entrou na fase 03 (room_03)!
 
-    Um novo arquivo apareceu...
+Um novo arquivo oculto apareceu...
 
-    === PRÓXIMO PASSO ===
-    Digite:
-    ls
+=== PRÓXIMO PASSO ===
+Digite:
+ls -a
 
-    Veja o novo arquivo
-    e descubra o que fazer.
+Veja o novo arquivo
+e descubra o que fazer.
 
-    === MISSÃO ===
-    Explore o arquivo que foi criado!
+=== MISSÃO ===
+Explore o arquivo que foi criado!
+
+▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
+▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
+▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
+
 "
 
   if command -v whiptail >/dev/null 2>&1; then
@@ -59,24 +91,33 @@ pwdd(){
 }
 
 if [[ ! -e "$engine_out/3/loaded.txt" ]]; then
-  mv "$dirfrom/instrução.txt" "$dirto" 2>/dev/null
+  mv "$dirfrom/.instrução.txt" "$dirto" 2>/dev/null
   echo "true" > $engine_out/3/loaded.txt
   clear
   pwdd
 else
   clear
   echo "
-    === FASE JÁ INICIADA ===
-    Você já está na fase 03 (room_03).
+▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
+▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
+▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
 
-    === PRÓXIMO PASSO ===
-    Digite:
-    ls
+=== FASE JÁ INICIADA ===
+Você já está na fase 03 (room_03).
 
-    Veja o novo arquivo
-    e descubra o que fazer.
+=== PRÓXIMO PASSO ===
+Digite:
+ls -a
 
-    === DICA ===
-    Use "ls" para encontrar novidades.
-    "
+Veja o novo arquivo
+e descubra o que fazer.
+
+=== DICA ===
+Use 'ls -a' para encontrar novidades.
+
+▄ ▄▖▄▖▖▖  ▖▖▖▖▖ ▖▄▖▄▖▄▖
+▙▘▌▌▚ ▙▌  ▙▌▌▌▛▖▌▐ ▙▖▙▘
+▙▘▛▌▄▌▌▌  ▌▌▙▌▌▝▌▐ ▙▖▌▌
+
+"
 fi
