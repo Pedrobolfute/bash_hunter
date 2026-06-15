ed212b94d6375b0049996457bd2fdeeeb72b14d9
Pedrobolfute <pepenike-101@hotmail.com>
2026-06-15 06:47:31 -0300

    fix

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def spawn_container():
       if wait_for_port(port):
         return jsonify({
           "success": True,
-          "url": f"http://bashhunter.com.br/play/{port}/"})
+          "url": f"https://bashhunter.com.br/play/{port}/"})
       else:
         subprocess.run(["docker", "stop", container_name], capture_output=True)
         with ports_lock:
