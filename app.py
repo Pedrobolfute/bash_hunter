12af1d9a8425c898740f7febd7e7f4a44c87b22f
Pedrobolfute <pepenike-101@hotmail.com>
2026-07-27 07:01:45 -0300

    back once -o again

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def spawn_container():
         "--cpus=0.5",
         "bash_hunter_image",
         "timeout", "120m",
-        "ttyd", "-p", "7681", "-m", "3", "-W",
+        "ttyd", "-p", "7681", "-o", "-W",
         "-t", "fontSize=14",
         "-b", f"/play/{port}",
         "/home/jogador/bash_hunter/.engine/init_game.sh"
